87a0311ee6b41e7a98ba2455c09b7b7ba50e7795
antonVosc <anton.vosc@gmail.com>
2025-01-10 15:34:35 +0100

    Worked on frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div id="chat-container"></div>
+  <form @submit.prevent="fetchAnswer" class="fixed-form">
+      <textarea rows="1" cols="1" placeholder="Ask GPT Model..." v-model="question"></textarea>
+      <button type="submit">
+        <img src="@/assets/send.svg" alt="send" />
+      </button>
+    </form>
   </main>
 </template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const question = ref("" );
+  const fetchAnswer = () => {
+    console.log(question.value);
+  }
+</script>
+
+<style>
+  .fixed-form {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    left: 30px;
+  }
+</style>
